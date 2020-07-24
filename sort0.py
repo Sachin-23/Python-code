ba5d6fb4f6eabefa1bf71d9089d1509eca1cb337
Sachin-23 <smudaliyar8452091824@gmail.com>
2020-07-24 17:32:59 +0530

    fix error

--- sort0.py
+++ sort0.py
@@ -34,4 +34,4 @@ print(sorted(l, key=lambda x: return 0 if n == 0 else max(l)))
 # Attempt 4
 
 l = [0, 1, 2, 10, 4, 1, 0, 56, 2, 0, 1, 3, 0, 56, 0, 4]
-print(sorted(l, key=lambda n: [0, max(l)][n == 0]))
+print(sorted(l, key=lambda n: [n, max(l) + 1][n == 0]))
